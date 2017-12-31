af5f38ed252703d84bd771260f907b6d849fb692
Haruki Takamura <s1526070sa@s.chibakoudai.jp>
2017-12-31 13:59:57 +0000

    Fixed bug

--- led.c
+++ led.c
@@ -3,7 +3,6 @@
  *
  *Copyright (C) 2017 Taishi Mikami <uroko100manen@gmail.com>
  *
- *
  *This program is free software: you can redistribute it and/or modify
  *it under the terms of the GNU General Public License as published by
  *the Free Software Foundation, either version 3 of the License, or
